b2bd221414b9f79b2313c7ecccfd76c9c85673ae
qadamboevvv07 <b07082009@gmail.com>
2026-02-11 17:26:33 +0500

    2 dars

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
-from .models import Category, News
 
-# Register your models here.
-admin.site.register(Category)
-admin.site.register(News)
+from app.models import News, Category
+
+
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'publish_time', 'status', 'category']
+    list_filter = ['title', 'category', 'publish_time']
+    prepopulated_fields = {'slug': ('title',)}
+    date_hierarchy = 'publish_time'
+    search_fields = ['']
+    ordering = ('-title',)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_filter = ['name']
