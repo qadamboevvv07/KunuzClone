ba21c4fdcb7373374338202f82088cc3b1079f8a
qadamboevvv07 <b07082009@gmail.com>
2026-02-13 19:30:14 +0500

    3 bolim muvofoqiyatli bajarildi

--- app/models.py
+++ app/models.py
@@ -1,31 +1,25 @@
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
-
 class Category(models.Model):
     name = models.CharField(max_length=150)
-
     def __str__(self):
         return self.name
 
-
 class News(models.Model):
     class Status(models.TextChoices):
-        DRAFT = 'DF', 'Draft'
+        Draft = 'DF', 'Draft'
         Published = 'PB', 'Published'
 
     title = models.CharField(max_length=300)
     slug = models.SlugField(max_length=300)
     body = models.TextField()
-    image = models.ImageField(upload_to='news/images', null=True)
+    image = models.ImageField(upload_to='news/images', null=True, blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     publish_time = models.DateTimeField(default=timezone.now)
     created_time = models.DateTimeField(auto_now_add=True)
     updated_time = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=2,
-                              choices=Status.choices,
-                              default=Status.DRAFT)
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.Draft)
 
     class Meta:
         ordering = ['-publish_time']
